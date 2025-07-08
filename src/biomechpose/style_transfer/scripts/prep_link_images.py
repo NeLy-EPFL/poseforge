c65d3ba48dd156f0c6c2b4fd1969969182678d26
Sibo Wang <sibo.wang@epfl.ch>
2025-07-08 13:38:18 +0200

    reserve test set

--- src/biomechpose/style_transfer/scripts/prep_link_images.py
+++ src/biomechpose/style_transfer/scripts/prep_link_images.py
@@ -12,17 +12,19 @@ if __name__ == "__main__":
     )
 
     linked_count = 0
-    exsits_count = 0
+    exist_count = 0
 
-    for trial_dir in all_preprocessed_images_dir.iterdir():
-        if not trial_dir.is_dir():
-            continue
-
+    trial_dirs = sorted(list(all_preprocessed_images_dir.iterdir()))
+    test_trials_dirs = trial_dirs[-1:]
+    for trial_dir in trial_dirs:
         # Process each image in the trial directory
         for image_path in trial_dir.glob("*.jpg"):
             trial_name = trial_dir.name
             # Create a symbolic link to the image in the output directory
-            new_image_path = unified_images_dir / (trial_name + "_" + image_path.name)
+            tag = "test" if trial_dir in test_trials_dirs else "train_and_validate"
+            new_image_path = (
+                unified_images_dir / tag / (trial_name + "_" + image_path.name)
+            )
             new_image_path.parent.mkdir(parents=True, exist_ok=True)
             if not new_image_path.exists():
                 new_image_path.symlink_to(image_path.resolve())
@@ -30,7 +32,7 @@ if __name__ == "__main__":
                 linked_count += 1
             else:
                 # print(f"Link already exists: {new_image_path}")
-                exsits_count += 1
+                exist_count += 1
 
     print(f"Linked {linked_count} images to {unified_images_dir}")
-    print(f"Skipped {exsits_count} images that already exist in {unified_images_dir}")
+    print(f"Skipped {exist_count} images that already exist in {unified_images_dir}")
