778af6afae947d15bf9a99aa4027711d33231b2e
Sibo Wang <sibo.wang@epfl.ch>
2025-09-13 12:15:13 +0200

    add script to count total duration of nmf-simulated data

---
+++ src/biomechpose/simulate_nmf/scripts/count_total_duration.py
@@ -0,0 +1,44 @@
+import cv2
+from pathlib import Path
+from tqdm import tqdm
+
+
+def check_num_frames(video_path: Path) -> int:
+    """Check number of frames in a video file"""
+    cap = cv2.VideoCapture(str(video_path))
+    if not cap.isOpened():
+        raise RuntimeError(f"Failed to open video file: {video_path}")
+    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+    cap.release()
+    return total_frames
+
+
+if __name__ == "__main__":
+    simulation_results_basedir = Path("bulk_data/nmf_rendering")
+    data_output_freq = 300  # Hz
+
+    section_video_files = simulation_results_basedir.rglob(
+        "segment_*/nmf_sim_render_colorcode_0.mp4"
+    )
+    section_video_files = list(sorted(section_video_files))
+    print("Checking results before postprocessing...")
+    total_frames = 0
+    for file in tqdm(section_video_files, disable=None):
+        total_frames += check_num_frames(file)
+    total_seconds = total_frames / data_output_freq
+    print(
+        f"Before postprocessing: {total_frames} total frames ({total_seconds:.2f} sec)"
+    )
+
+    subsection_video_files = simulation_results_basedir.rglob(
+        "subsegment_*/processed_nmf_sim_render_colorcode_0.mp4"
+    )
+    subsection_video_files = list(sorted(subsection_video_files))
+    print("Checking results after postprocessing...")
+    total_frames = 0
+    for file in tqdm(subsection_video_files, disable=None):
+        total_frames += check_num_frames(file)
+    total_seconds = total_frames / data_output_freq
+    print(
+        f"After postprocessing: {total_frames} total frames ({total_seconds:.2f} sec)"
+    )
