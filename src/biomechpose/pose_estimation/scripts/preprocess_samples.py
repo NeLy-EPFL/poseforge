f0e02b6eee73b0bc083786991227baf526452df5
Sibo Wang <sibo.wang@epfl.ch>
2025-09-23 12:33:24 +0200

    Make atomic batch extraction a CLI for cluster processing

--- src/biomechpose/pose_estimation/scripts/preprocess_samples.py
+++ src/biomechpose/pose_estimation/scripts/preprocess_samples.py
@@ -12,22 +12,71 @@ from biomechpose.pose_estimation.sampler import (
 )
 
 
-if __name__ == "__main__":
-    # Parameters
-    atomic_batch_nframes = 32
-    atomic_batch_nvariants_max = 4
-    minimum_time_diff_frames = 60  # 0.2s at 300Hz
-    nmf_sim_rendering_basedir = Path("bulk_data/nmf_rendering/")
-    output_dir = Path("bulk_data/pose_estimation/atomic_batches")
-    n_jobs = -1  # number of parallel jobs, -1 == use all CPUs, 0 == serial
-    logging_interval = 100
+def extract_atomic_batches(
+    atomic_batch_nframes: int,
+    atomic_batch_nvariants_max: int,
+    minimum_time_diff_frames: int,
+    input_basedir: Path | str,
+    nmf_sim_rendering_basedir: Path | str,
+    output_dir: Path | str,
+    n_jobs: int = -1,
+    logging_interval: int = 100,
+    cache_metadata: bool = True,
+    use_cached_metadata: bool = True,
+    video_spacing: int = 10,
+) -> None:
+    """Extract atomic batches from simulated data. This makes data loading
+    faster during pose estimation training.
+
+    Each "atomic batch" contains a small number of frames translated into
+    the domain of experimental data using a number of different style
+    transfer models (each referred to as a "variant"). Each batch therefore
+    contains:
+        - Simulated frames: n_variants * n_frames, saved as a video of
+          n_frames frames. In each frame of the video, the n_variants
+          variants are tiled horizontally with a fixed spacing of
+          `video_spacing` pixels between each variant.
+        - Simulated data: a single H5 file containing the simulated data
+          (3D joint positions, etc.) for these frames (note that different
+          variants share the same simulated data since they come from the
+          same NeuroMechFly simulation).
+
+    Args:
+        atomic_batch_nframes (int): The number of frames in each atomic
+            batch.
+        atomic_batch_nvariants_max (int): The maximum number of variants to
+            include in each atomic batch.
+        minimum_time_diff_frames (int): The minimum time difference between
+            frames for them to be considered different (and therefore
+            "randomly" sampled).
+        input_basedir (Path | str): The base directory containing synthetic
+            videos.
+        nmf_sim_rendering_basedir (Path | str): The base directory for NMF
+            simulation rendering.
+        output_dir (Path | str): The output directory for atomic batches.
+        n_jobs (int, optional): The number of jobs to run in parallel. 0 =
+            run in a plain loop; -1 = use all available cores. Defaults to
+            -1.
+        logging_interval (int, optional): The interval at which to log
+            progress. Defaults to 100.
+        cache_metadata (bool, optional): Whether to cache metadata when
+            indexing simulations info (e.g. length of simulation). Defaults
+            to True.
+        use_cached_metadata (bool, optional): Whether to use cached
+            metadata if they are available. Defaults to True.
+        video_spacing (int, optional): The spacing (in pixels) between
+            variants in the output video. Defaults to 10.
+    """
+    input_basedir = Path(input_basedir)
+    nmf_sim_rendering_basedir = Path(nmf_sim_rendering_basedir)
+    output_dir = Path(output_dir)
+    output_dir.mkdir(parents=True, exist_ok=True)
 
     # Set the following to a non-None value to limit the scope of the job for testing
     max_n_simulations: int | None = None
     max_n_variants: int | None = None
 
     # Define all synthetic videos
-    input_basedir = Path("bulk_data/style_transfer/production/translated_videos")
     assert input_basedir.is_dir(), f"`input_basedir` {input_basedir} is not a directory"
     input_simulation_paths = sorted(list(input_basedir.rglob("subsegment_*")))
     if max_n_simulations is not None:
@@ -60,6 +109,8 @@ if __name__ == "__main__":
             synthetic_video_paths,
             simulated_labels_path,
             sim_name=f"{exp_trial}/{segment}/{subsegment}",
+            cache_metadata=cache_metadata,
+            use_cached_metadata=use_cached_metadata,
         )
 
     simulated_data_sequences = Parallel(n_jobs=-1)(
@@ -96,7 +147,7 @@ if __name__ == "__main__":
 
     # Define payload
     start_time = time()
-    
+
     def process_batch(batch_idx: int):
         frames, labels = sampler[batch_idx]
         # Split into multiple atomic batches if n_variants > atomic_batch_nvariants_max
@@ -113,6 +164,7 @@ if __name__ == "__main__":
                 atomic_batch_frames,
                 output_dir / f"{filename_stem}_frames.mp4",
                 fps=sampler.fps,
+                spacing=video_spacing,
             )
             save_atomic_batch_sim_data(
                 labels, output_dir / f"{filename_stem}_labels.h5"
@@ -140,3 +192,33 @@ if __name__ == "__main__":
             for i_batch in trange(len(sampler), disable=None)
         )
     print("All done")
+
+
+if __name__ == "__main__":
+    # CLI
+    import tyro
+
+    tyro.cli(extract_atomic_batches)
+    # CLI example:
+    # python src/biomechpose/pose_estimation/scripts/preprocess_samples.py \
+    #     --atomic-batch-nframes 32 \
+    #     --atomic-batch-nvariants-max 4 \
+    #     --minimum-time-diff-frames 60 \
+    #     --input-basedir bulk_data/style_transfer/production/translated_videos/BO_Gal4_fly1_trial001 \
+    #     --nmf-sim-rendering-basedir bulk_data/nmf_rendering \
+    #     --output-dir bulk_data/pose_estimation/atomic_batches
+
+    # # Example usage not via CLI
+    # extract_atomic_batches(
+    #     atomic_batch_nframes=32,
+    #     atomic_batch_nvariants_max=4,
+    #     minimum_time_diff_frames=60,
+    #     input_basedir="bulk_data/style_transfer/production/translated_videos",
+    #     nmf_sim_rendering_basedir="bulk_data/nmf_rendering/",
+    #     output_dir="bulk_data/pose_estimation/atomic_batches",
+    #     n_jobs=-1,
+    #     logging_interval=100,
+    #     cache_metadata=True,
+    #     use_cached_metadata=True,
+    #     video_spacing=10,
+    # )
