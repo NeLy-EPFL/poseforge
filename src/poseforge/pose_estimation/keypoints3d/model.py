[Fix] fix heatmap size calculation bug

src/poseforge/pose_estimation/keypoints3d/model.py
@@ -435,6 +435,7 @@ class Pose2p5DModel(nn.Module):
 
         # Map to input image pixel coordinates (input images are 256x256, but heatmaps
         # are predicted at 64x64, so there is a stride of 4)
+        heatmap_size = heatmaps.shape[-2:]  # (n_rows_out, n_cols_out)
         stride = self.feature_extractor.input_size[0] / heatmap_size[0]
         assert stride == 4, "Expected input size=256x256 and output heatmap size=64x64"
         xy_px_in = xy_px_out * stride  # (N, n_keypoints, 2)
@@ -461,7 +462,6 @@ class Pose2p5DModel(nn.Module):
             assert d2.shape == (batch_size, 128, 32, 32)
             assert d1.shape == (batch_size, self.upsample_core_out_channels, 64, 64)
 
-            heatmap_size = heatmaps.shape[-2:]
             assert heatmaps.shape == (batch_size, self.n_keypoints, *heatmap_size)
             assert xy_px_in.shape == (batch_size, self.n_keypoints, 2)
             assert xy_conf.shape == (batch_size, self.n_keypoints)