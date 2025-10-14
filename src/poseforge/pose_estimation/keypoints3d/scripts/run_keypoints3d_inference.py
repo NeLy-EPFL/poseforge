ec5b8be2235cf2570d72f978e61baf0fdfe2c1ac
Sibo Wang <sibo.wang@epfl.ch>
2025-10-14 14:48:33 +0200

    [Run] update epoch numbers

--- src/poseforge/pose_estimation/keypoints3d/scripts/run_keypoints3d_inference.py
+++ src/poseforge/pose_estimation/keypoints3d/scripts/run_keypoints3d_inference.py
@@ -174,7 +174,7 @@ def run_keypoints3d_inference(
 if __name__ == "__main__":
     input_basedir = Path("bulk_data/behavior_images/spotlight_aligned_and_cropped/")
     model_dir = Path("bulk_data/pose_estimation/keypoints3d/trial_20251013b")
-    epochs_to_try = list(range(0, 10, 2))
+    epochs_to_try = list(range(0, 30, 2))
 
     for epoch in epochs_to_try:
         print(f"Running inference for epoch {epoch}")
