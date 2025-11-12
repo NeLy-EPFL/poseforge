a69ddb3b0a079890e878be5332f80cc7c9e2e078
Sibo Wang-Chen <sibo.wang@epfl.ch>
2025-11-12 19:47:42 +0100

    Run keypoints3d model at output stride of 2 (instead of 4) to improve accuracy (#39)
    * [Feature] run keypoints3d model at output stride of 2 (instead of 4) to improve accuracy
    
    * [Run/fix] Fix bug with keypoints3d training config and rerun training with output stride 2 instead of 4
    
    * [Doc] fix doc

--- src/poseforge/pose/keypoints3d/pipeline.py
+++ src/poseforge/pose/keypoints3d/pipeline.py
@@ -302,6 +302,7 @@ class Pose2p5DPipeline:
             {
                 "params": list(
                     chain(
+                        self.model.dec_layer1.parameters(),
                         self.model.dec_layer2.parameters(),
                         self.model.dec_layer3.parameters(),
                         self.model.dec_layer4.parameters(),
